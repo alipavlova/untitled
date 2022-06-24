5e41959a41d8cbbc1bd5ff5bdb3a2f13014ef39b
Alisa Pavlova <alipavlova2020@gmail.com>
2022-06-24 17:57:21 +0300

    init

---
+++ src/Main.java
@@ -0,0 +1,7 @@
+public class Main {
+    public static void main(String[] args) {
+        int amount = 89;
+        int miles = amount / 20;
+        System.out.println("бонусные мили = " + miles);
+    }
+}
